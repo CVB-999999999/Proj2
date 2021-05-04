f279f6f8b1e073570489b2f8011c070ddcf45f00
pdanielferreira <62609011+pdanielferreira@users.noreply.github.com>
2021-05-04 12:11:32 +0100

    1.3

--- src/Proj2/main.java
+++ src/Proj2/main.java
@@ -1,6 +1,6 @@
 package Proj2;
 
-public class main {
+/*public class main {
     public static void main(String[] args) {
         cliente c = new cliente();
 
@@ -14,10 +14,92 @@ public class main {
         c.setTelemovel("99999999999");
         c.setRua("99999999999");
 
-        c.create();*/
+        c.create();
         c.read(65);
         System.out.println(c.getIdCliente() + " " + c.getNome());
 
-        tabelaClientes tb = new tabelaClientes();
+        tabelaClientes listarCli = new tabelaClientes();
+        this.painelVisivel = listarCli;
+
+    }
+}*/
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+
+public class main extends JFrame {
+
+    private JMenuBar barraMenu;
+    private JMenu menuCliente;
+    private JMenuItem menuCriarCli;
+    private JMenuItem menuListarCli;
+
+    public main(){
+        menuCriarCli = new JMenuItem("Criar");
+        menuListarCli = new JMenuItem("Listar");
+        menuCliente = new JMenu();
+        barraMenu = new JMenuBar();
+
+        menuCliente.setText("Cliente");
+        //menuCliente.add(menuCriarCli);
+        menuCliente.add(menuListarCli);
+        barraMenu.add(menuCliente);
+
+        /*menuCriarCli.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                criarClienteActionPerformed(e);
+            }
+        });*/
+
+        menuListarCli.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listarClienteActionPerformed(e);
+            }
+        });
+
+        this.setJMenuBar(barraMenu);
+
+       /* WelcomeForm wform = new WelcomeForm();
+        this.getContentPane().add(wform.getWelcomePanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+    }
+
+    /*public void criarClienteActionPerformed(java.awt.event.ActionEvent e)
+    {
+        EditCliente form = new EditCliente();
+
+        form.getpEditCliente().setVisible(true);
+        this.setContentPane(form.getpEditCliente());
+        this.pack();
+        //this.getContentPane().add(form.getpEditCliente());
+        this.repaint();
+
+    }
+*/
+    public void listarClienteActionPerformed(java.awt.event.ActionEvent e)
+    {
+        try {
+            Proj2.tabelaClientes lista;
+            lista = new Proj2.tabelaClientes();
+            lista.getPanel1().setVisible(true);
+            this.setContentPane(lista.getPanel1());
+            this.pack();
+            this.repaint();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        main frame = new main();
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/Proj2/teste.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class teste {
+    private JButton listarClientesButton;
+}
