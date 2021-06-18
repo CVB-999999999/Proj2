e0ab05e2be030ca3f25abd0abbb5c5e84a5d6a7b
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2021-06-18 22:33:47 +0100

    wdf

---
+++ web/src/test/java/proj2/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package proj2.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
